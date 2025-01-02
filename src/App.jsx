7853974cad95f0135a7033bfefb19c8ae6257532
Lucky Rajput <124134123+Lucky012001@users.noreply.github.com>
2025-01-02 12:53:45 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+ 
  
 import './App.css'
 
@@ -13,7 +13,7 @@ function App() {
          
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+         
         </a>
       </div>
       <h1>Vite + React</h1>
