4c49fec7be130d0cc93b8c89ea7ea0a43cf7d9c7
Lucky Rajput <124134123+Lucky012001@users.noreply.github.com>
2025-01-04 18:00:08 +0530

    changes

--- src/Layout/Navbar.jsx
+++ src/Layout/Navbar.jsx
@@ -10,15 +10,15 @@ const Navbar = () => {
                 <li><a href="/about">About</a></li>
                 <li><a href="/contact">Contact</a></li>
                 <li class="dropdown">
-  <a href="/contact" class="dropdown-toggle">Manage Account</a>
-  <ul class="dropdown-menu">
-    <li><a href="/doctor"> Doctor Manage</a></li>
-    <li><a href="/staff"> Staff Manage</a></li>
-    <li><a href="/patient"> Patient Manage</a></li>
-  </ul>
-</li>
+                    <a href="/contact" class="dropdown-toggle">Manage Account</a>
+                    <ul class="dropdown-menu">
+                        <li><a href="/doctor"> Doctor Manage</a></li>
+                        <li><a href="/staff"> Staff Manage</a></li>
+                        <li><a href="/patient"> Patient Manage</a></li>
+                    </ul>
+                </li>
 
-            </ul>
+            </ul> 
         </nav>
     );
 };
