845749e2f91a5fce6d2e3aa80428681e625e9b3d
Lucky Rajput <124134123+Lucky012001@users.noreply.github.com>
2025-01-09 14:50:49 +0530

    update code

--- src/components/About us/Aboutus.jsx
+++ src/components/About us/Aboutus.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import Layout from '../../Layout/Layout'
+import React from 'react';
+import Layout from '../../Layout/Layout';
+
+import './Aboutus.css'; // CSS file for styling and animations
 
 const Aboutus = () => {
     return (
         <Layout>
-            <div className='.dashboard-container'>
-                <div>Aboutus</div>
+            <div className="dashboard-container">
+                <div className="welcome-section">
+                    <h1 className="welcome-text">
+                        "Welcome to Swasthya Rakshak, your trusted partner in health and well-being!"
+                    </h1>
+                    <p className="welcome-subtext">
+                        Join us in our mission to safeguard your health with care, innovation, and commitment.
+                    </p>
+                </div>
             </div>
 
         </Layout>
-    )
-}
+    );
+};
 
-export default Aboutus
+export default Aboutus;
